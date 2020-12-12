57554f327bc6ce2bf9103e7341b3047a577e0f97
Micah Resnick <resnickmicah@gmail.com>
2020-12-11 21:06:44 -0500

    Finish d2p1

--- day2/day2.go
+++ day2/day2.go
@@ -32,9 +32,9 @@ func fileToFilters(path string) ([]PasswordPolicy, error) {
 func tokenize(policyLine string) PasswordPolicy {
 	splitPolicy := strings.Split(policyLine, ":")
 	filter, password := splitPolicy[0], splitPolicy[1]
-	strings.TrimSpace(filter)
-	strings.TrimSpace(password)
-	splitFilter := strings.Split(policyLine, " ")
+	filter = strings.TrimSpace(filter)
+	password = strings.TrimSpace(password)
+	splitFilter := strings.Split(filter, " ")
 	lowHigh, filterChar := splitFilter[0], splitFilter[1]
 	splitRange := strings.Split(lowHigh, "-")
 	lowInt, lowErr := strconv.Atoi(splitRange[0])
@@ -46,10 +46,47 @@ func tokenize(policyLine string) PasswordPolicy {
 		panic(errMsg)
 	}
 	retval := PasswordPolicy{low, high, filterChar, password}
-	fmt.Printf("Password policy as struct: %s\n", retval)
 	return retval
 }
 
+func processPolicies(policies []PasswordPolicy) (uint16) {
+	var compliantPasswords uint16
+	for _, policy := range policies {
+		charMap := getCharCounts(policy.password)
+		if(checkCompliance(charMap, policy)) {
+			compliantPasswords += 1
+		}
+	}
+	return compliantPasswords
+}
+
+func getCharCounts(password string) (map[string]uint8) {
+	passwordCharCount := map[string]uint8{}
+	for _, char := range strings.Split(password, "") {
+		if _, ok:= passwordCharCount[char]; ok {
+			passwordCharCount[char] += 1
+		} else {
+			passwordCharCount[char] = 1
+		}
+	}
+	return passwordCharCount
+}
+
+func checkCompliance(passwordCharCount map[string]uint8, currentPolicy PasswordPolicy) (bool) {
+	var filterCharCount uint8
+	if _, ok := passwordCharCount[currentPolicy.filterChar]; ok {
+		filterCharCount = passwordCharCount[currentPolicy.filterChar]
+		return currentPolicy.low <= filterCharCount && currentPolicy.high >= filterCharCount
+	} else {
+		return false
+	}
+}
+
 func main() {
-	fileToFilters("./day2_input.txt")
+	filters, err := fileToFilters("./day2_input.txt")
+	if err != nil {
+		panic("Your file is bad and you should feel bad")
+	}
+	compliantPasswords := processPolicies(filters)
+	fmt.Printf("Number of compliant passwords: %i\n", compliantPasswords)
 }
